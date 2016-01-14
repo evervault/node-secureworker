18433e5bdf903e7dcfbc83bf610e19eccdb56163
w <none>
2016-01-13 16:47:06 -0800

    ndoe module to interact with enclave

--- duktape-enclave/duktape-enclave.cpp
+++ duktape-enclave/duktape-enclave.cpp
@@ -3,11 +3,19 @@
 #include "sgx_trts.h"
 #include "duktape.h"
 
-double test() {
-	duk_context *ctx = duk_create_heap_default();
+duk_context *ctx = NULL;
+
+void duk_enclave_init() {
+	ctx = duk_create_heap_default();
+}
+
+void duk_enclave_close() {
+	duk_destroy_heap(ctx);
+}
+
+double duk_enclave_test() {
 	duk_eval_string(ctx, "9 * 8");
 	const double result = duk_to_number(ctx, -1);
 	duk_pop(ctx);
-	duk_destroy_heap(ctx);
 	return result;
 }
---
+++ run.js
@@ -0,0 +1,2 @@
+var m = require('./Release/m');
+console.log(m.test());
