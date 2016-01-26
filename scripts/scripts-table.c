4b51330cdbe40f17e9672d1da16c5aebea4d23a8
w <none>
2016-01-26 11:16:50 -0800

    go easy on the linker

---
+++ scripts/scripts-table.c
@@ -0,0 +1,8 @@
+#include "scripts.h"
+extern const char binary_main_js_size[], binary_main_js_start[], binary_main_js_end[];
+extern const char binary_framework_js_size[], binary_framework_js_start[], binary_framework_js_end[];
+const size_t MAX_SCRIPT = 2;
+const duk_enclave_script_t SCRIPTS[2] = {
+	{(size_t) binary_main_js_size, binary_main_js_start, binary_main_js_end},
+	{(size_t) binary_framework_js_size, binary_framework_js_start, binary_framework_js_end},
+};
