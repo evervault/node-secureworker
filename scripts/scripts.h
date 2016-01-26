09b47567f1e9c95ca2bbe228a06bc2a22f884a49
w <none>
2016-01-26 11:16:50 -0800

    go easy on the linker

--- scripts/scripts.h
+++ scripts/scripts.h
@@ -1,7 +1,14 @@
 #include <stddef.h>
-struct duk_enclave_script {
-	const char * start;
+typedef struct duk_enclave_script {
 	size_t size;
-};
+	const char * start;
+	const char * end;
+} duk_enclave_script_t;
+#ifdef __cplusplus
+extern "C" {
+#endif
 extern const size_t MAX_SCRIPT;
-extern const duk_enclave_script SCRIPTS[];
+extern const duk_enclave_script_t SCRIPTS[];
+#ifdef __cplusplus
+}
+#endif
