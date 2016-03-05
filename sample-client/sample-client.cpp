02fbf886cecab6219c5a9917af66f0b91d3ce958
w <none>
2016-03-04 18:38:08 -0800

    Add interfaces for platform service functions.

--- sample-client/sample-client.cpp
+++ sample-client/sample-client.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 
+#include "sgx_uae_service.h"
 #include "sgx_urts.h"
 #include "duk_enclave_u.h"
 
@@ -18,6 +19,39 @@ void duk_enclave_post_message(const char *message) {
 
 void duk_enclave_debug(const char *message) {
 	std::cerr << message << std::endl;
+}
+
+void duk_enclave_init_quote(sgx_target_info_t *p_target_info) {
+	{
+		sgx_epid_group_id_t gid;
+		const sgx_status_t status = sgx_init_quote(p_target_info, &gid);
+		if (status != SGX_SUCCESS) {
+			std::cerr << "sgx_init_quote failed" << std::endl;
+			exit(status);
+		}
+	}
+}
+
+void duk_enclave_post_quote_report(sgx_report_t *report) {
+	uint32_t quote_size;
+	{
+		const sgx_status_t status = sgx_get_quote_size(NULL, &quote_size);
+		if (status != SGX_SUCCESS) {
+			std::cerr << "sgx_get_quote_size failed" << std::endl;
+			exit(status);
+		}
+	}
+	char *quote_memory = new char[quote_size];
+	sgx_quote_t *quote = reinterpret_cast<sgx_quote_t *>(quote_memory);
+	{
+		const sgx_spid_t spid = {""};
+		const sgx_status_t status = sgx_get_quote(report, SGX_UNLINKABLE_SIGNATURE, &spid, NULL, NULL, 0, NULL, quote, quote_size);
+		if (status != SGX_SUCCESS) {
+			std::cerr << "sgx_get_quote failed" << std::endl;
+			exit(status);
+		}
+	}
+	std::cout << "received quote" << std::endl;
 }
 
 int _tmain(int argc, _TCHAR* argv[]) {
