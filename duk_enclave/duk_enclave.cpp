ff04098c8c001b4afca4a057bf846d26e14a5c2c
w <none>
2016-03-15 04:10:18 -0700

    Do attestation stuff natively.

--- duk_enclave/duk_enclave.cpp
+++ duk_enclave/duk_enclave.cpp
@@ -5,6 +5,7 @@
 #endif
 #include "sgx_tae_service.h"
 #include "sgx_tcrypto.h"
+#include "sgx_tkey_exchange.h"
 #include "sgx_trts.h"
 #include "sgx_tseal.h"
 #include "sgx_utils.h"
@@ -122,76 +123,6 @@ static duk_ret_t native_debug(duk_context *ctx) {
 	const char * const arg = duk_safe_to_string(ctx, 0);
 	output_debug_line(arg);
 	return 0;
-}
-
-static duk_ret_t native_post_quote(duk_context *ctx) {
-	duk_size_t report_data_size;
-	const void * const report_data = duk_get_buffer_data(ctx, 0, &report_data_size);
-	if (report_data_size != sizeof(sgx_report_data_t)) return DUK_RET_ERROR;
-	sgx_target_info_t qe_target_info;
-	{
-		const sgx_status_t status = duk_enclave_init_quote(&qe_target_info);
-		if (status != SGX_SUCCESS) throw_sgx_status(ctx, status, "duk_enclave_init_quote");
-	}
-	sgx_report_t report;
-	{
-		const sgx_status_t status = sgx_create_report(
-			&qe_target_info,
-			reinterpret_cast<const sgx_report_data_t *>(report_data),
-			&report
-		);
-		if (status != SGX_SUCCESS) throw_sgx_status(ctx, status, "sgx_create_report");
-	}
-	{
-		const sgx_status_t status = duk_enclave_post_quote_report(&report);
-		if (status != SGX_SUCCESS) throw_sgx_status(ctx, status, "duk_enclave_post_quote_report");
-	}
-	return 0;
-}
-
-static duk_ret_t native_seal_data(duk_context *ctx) {
-	duk_size_t additional_data_size;
-	const void * const additional_data = duk_get_buffer_data(ctx, 0, &additional_data_size);
-	duk_size_t data_size;
-	const void * const data = duk_get_buffer_data(ctx, 1, &data_size);
-	const uint32_t sealed_data_size = sgx_calc_sealed_data_size(additional_data_size, data_size);
-	void * const sealed_data = duk_push_fixed_buffer(ctx, sealed_data_size);
-	{
-		const sgx_status_t status = sgx_seal_data(
-			additional_data_size, reinterpret_cast<const uint8_t *>(additional_data),
-			data_size, reinterpret_cast<const uint8_t *>(data),
-			sealed_data_size, reinterpret_cast<sgx_sealed_data_t *>(sealed_data)
-		);
-		if (status != SGX_SUCCESS) throw_sgx_status(ctx, status, "sgx_seal_data");
-	}
-	duk_push_buffer_object(ctx, -1, 0, sealed_data_size, DUK_BUFOBJ_ARRAYBUFFER);
-	return 1;
-}
-
-static duk_ret_t native_unseal_data(duk_context *ctx) {
-	duk_size_t sealed_data_size;
-	const void * const sealed_data = duk_get_buffer_data(ctx, 0, &sealed_data_size);
-	uint32_t additional_data_size = sgx_get_add_mac_txt_len(reinterpret_cast<const sgx_sealed_data_t *>(sealed_data));
-	if (additional_data_size == UINT32_MAX) return DUK_RET_ERROR;
-	uint32_t data_size = sgx_get_encrypt_txt_len(reinterpret_cast<const sgx_sealed_data_t *>(sealed_data));
-	if (data_size == UINT32_MAX) return DUK_RET_ERROR;
-	void * const additional_data = duk_push_fixed_buffer(ctx, additional_data_size);
-	void * const data = duk_push_fixed_buffer(ctx, data_size);
-	{
-		const sgx_status_t status = sgx_unseal_data(
-			reinterpret_cast<const sgx_sealed_data_t *>(sealed_data),
-			reinterpret_cast<uint8_t *>(additional_data), &additional_data_size,
-			reinterpret_cast<uint8_t *>(data), &data_size
-		);
-		if (status != SGX_SUCCESS) throw_sgx_status(ctx, status, "sgx_seal_data");
-	}
-	duk_push_object(ctx);
-	// [additional_data plain buffer] [data plain buffer] [object]
-	duk_push_buffer_object(ctx, -3, 0, additional_data_size, DUK_BUFOBJ_ARRAYBUFFER);
-	duk_put_prop_string(ctx, -2, "additionalData");
-	duk_push_buffer_object(ctx, -2, 0, data_size, DUK_BUFOBJ_ARRAYBUFFER);
-	duk_put_prop_string(ctx, -2, "data");
-	return 1;
 }
 
 static duk_ret_t native_get_time(duk_context *ctx) {
@@ -537,9 +468,6 @@ static const duk_function_list_entry native_methods[] = {
 	{"nextTick", native_next_tick, 1},
 	{"importScript", native_import_script, 1},
 	{"debug", native_debug, 1},
-	{"postQuote", native_post_quote, 1},
-	{"sealData", native_seal_data, 2},
-	{"unsealData", native_unseal_data, 1},
 	{"getTime", native_get_time, 0},
 	{"createMonotonicCounter", native_create_monotonic_counter, 0},
 	{"destroyMonotonicCounter", native_destroy_monotonic_counter, 1},
@@ -576,14 +504,22 @@ static void spin_microtasks(duk_context *ctx) {
 	duk_pop_3(ctx);
 }
 
+static enum {
+	NASCENT,
+	ACTIVE,
+	CLOSED,
+	RA,
+} state = NASCENT;
+
 static duk_context *ctx = NULL;
 
 void duk_enclave_init(const char *key) {
-	if (ctx != NULL) abort();
+	if (state != NASCENT) abort();
+	state = ACTIVE;
 	ctx = duk_create_heap(NULL, NULL, NULL, NULL, report_fatal);
 	if (ctx == NULL) abort();
 	{
-		sgx_status_t status = sgx_create_pse_session();
+		const sgx_status_t status = sgx_create_pse_session();
 		if (status != SGX_SUCCESS) abort();
 	}
 	// Framework code
@@ -608,17 +544,18 @@ void duk_enclave_init(const char *key) {
 }
 
 void duk_enclave_close() {
-	if (ctx == NULL) abort();
+	if (state != ACTIVE) abort();
+	state = CLOSED;
 	duk_destroy_heap(ctx);
 	ctx = NULL;
 	{
-		sgx_status_t status = sgx_close_pse_session();
+		const sgx_status_t status = sgx_close_pse_session();
 		if (status != SGX_SUCCESS) abort();
 	}
 }
 
 void duk_enclave_emit_message(const char *message) {
-	if (ctx == NULL) abort();
+	if (state != ACTIVE) abort();
 	duk_get_global_string(ctx, "_dukEnclaveHandlers");
 	duk_get_prop_string(ctx, -1, "emitMessage");
 	duk_push_string(ctx, message);
@@ -626,4 +563,27 @@ void duk_enclave_emit_message(const char *message) {
 	if (result == DUK_EXEC_ERROR) report_error(ctx);
 	duk_pop(ctx);
 	spin_microtasks(ctx);
+}
+
+void duk_enclave_init_ra(sgx_ra_context_t *ra_context, int pse) {
+	if (state != NASCENT) abort();
+	state = RA;
+	if (pse) {
+		const sgx_status_t status = sgx_create_pse_session();
+		if (status != SGX_SUCCESS) abort();
+	}
+	const duk_enclave_script_t *sp_public_key = look_up_script("sp.private.raw");
+	if (sp_public_key == NULL) abort();
+	{
+		const sgx_status_t status = sgx_ra_init(
+			reinterpret_cast<const sgx_ec256_public_t *>(sp_public_key->start),
+			pse,
+			ra_context
+		);
+		if (status != SGX_SUCCESS) abort();
+	}
+	if (pse) {
+		const sgx_status_t status = sgx_close_pse_session();
+		if (status != SGX_SUCCESS) abort();
+	}
 }
