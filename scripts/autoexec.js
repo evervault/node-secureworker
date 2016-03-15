077b3ba7bfa472baccb808773a17a82338f6a004
w <none>
2016-03-15 05:30:00 -0700

    Move autoexec to separate directory

--- scripts/autoexec.js
+++
@@ -1,3 +0,0 @@
-var setTimeout; // so Promise.js doesn't crash
-_dukEnclaveNative.importScript('Promise.js');
-_dukEnclaveNative.importScript('framework.js');
---
+++ scripts/framework/autoexec.js
@@ -0,0 +1,3 @@
+var setTimeout; // so Promise.js doesn't crash
+_dukEnclaveNative.importScript('../framework/Promise.js');
+_dukEnclaveNative.importScript('../framework/framework.js');
