612bb168e6f47e193a67a53decdc2e59cc5ad05f
w <none>
2016-03-30 06:32:23 -0700

    Change whitespace in scripts.

--- scripts/main.js
+++ scripts/main.js
@@ -6,18 +6,18 @@ F.postMessage(basic);
 
 var rawKeyHex = '2442A5CC0ECD015FA3CA31DC8E2BBC70BF42D60CBCA20085E0822CB04235E9706FC98BD7E50211A4A27102FA3549DF79EBCB4BF246B80945CDDFE7D509BBFD7D';
 var publicKey = crypto.subtle.importKey('raw-public-uncompressed-be', Duktape.dec('hex', rawKeyHex), {
-    name: 'ECDSA',
-    namedCurve: 'P-256',
+	name: 'ECDSA',
+	namedCurve: 'P-256',
 }, true, ['verify']);
 F.onMessage(function (message) {
-    publicKey.then(function (publicKey) {
-        return crypto.subtle.verify({
-            name: 'ECDSA',
-            hash: 'SHA-256',
-        }, publicKey, Duktape.dec('hex', message.signature), Duktape.dec('hex', message.data));
-    }).then(function (valid) {
-        F.postMessage(valid);
-    }).catch(function (reason) {
-        console.log('rejected', reason.stack || reason);
-    });
+	publicKey.then(function (publicKey) {
+		return crypto.subtle.verify({
+			name: 'ECDSA',
+			hash: 'SHA-256',
+		}, publicKey, Duktape.dec('hex', message.signature), Duktape.dec('hex', message.data));
+	}).then(function (valid) {
+		F.postMessage(valid);
+	}).catch(function (reason) {
+		console.log('rejected', reason.stack || reason);
+	});
 });
