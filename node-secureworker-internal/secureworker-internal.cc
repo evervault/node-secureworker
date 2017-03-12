be5f811d9c4a3088a051c9971317ecd49785b547
Mitar <mitar.git@tnode.com>
2017-03-11 16:30:52 -0800

    Converted C code to nan.

--- node-secureworker-internal/secureworker-internal.cc
+++ node-secureworker-internal/secureworker-internal.cc
@@ -1,5 +1,4 @@
-#include <node.h>
-#include <v8_typed_array.h>
+#include <nan.h>
 
 #include <iomanip>
 #include <iostream> // %%%
@@ -23,8 +22,8 @@ entry_info *thread_entry = nullptr;
 
 struct entry_info {
 	entry_info *previous;
-	v8::Handle<v8::Object> entrant;
-	entry_info(v8::Handle<v8::Object> entrant) : previous(thread_entry), entrant(entrant) {
+	v8::Local<v8::Object> entrant;
+	entry_info(v8::Local<v8::Object> entrant) : previous(thread_entry), entrant(entrant) {
 		thread_entry = this;
 	}
 	~entry_info() {
@@ -43,28 +42,32 @@ struct sgx_error {
 	void rethrow() {
 		std::stringstream ss;
 		ss << source << " failed (0x" << std::hex << std::setw(4) << std::setfill('0') << status << ")";
-		v8::ThrowException(v8::Exception::Error(v8::String::New(ss.str().c_str())));
+		Nan::ThrowError(ss.str().c_str());
 	}
 };
 
 // The rest of the stuff, which is per-instance
 
-class SecureWorkerInternal : public node::ObjectWrap {
+class SecureWorkerInternal : public Nan::ObjectWrap {
 public:
 	sgx_enclave_id_t enclave_id;
+
 	explicit SecureWorkerInternal(const char *file_name);
 	~SecureWorkerInternal();
+
 	void init(const char *key);
 	void close();
 	void emitMessage(const char *message);
 	void bootstrapMock(sgx_sealed_data_t *out, size_t out_size,
 	                   const uint8_t *additional_data, size_t additional_data_size,
-	                   const uint8_t *clear, size_t data_size);
-	static v8::Handle<v8::Value> New(const v8::Arguments &arguments);
-	static v8::Handle<v8::Value> Init(const v8::Arguments &arguments);
-	static v8::Handle<v8::Value> Close(const v8::Arguments &arguments);
-	static v8::Handle<v8::Value> EmitMessage(const v8::Arguments &arguments);
-	static v8::Handle<v8::Value> BootstrapMock(const v8::Arguments &arguments);
+	                   const uint8_t *data, size_t data_size);
+
+	static Nan::Persistent<v8::Function> constructor;
+	static NAN_METHOD(New);
+	static NAN_METHOD(Init);
+	static NAN_METHOD(Close);
+	static NAN_METHOD(EmitMessage);
+	static NAN_METHOD(BootstrapMock);
 };
 
 SecureWorkerInternal::SecureWorkerInternal(const char *file_name) : enclave_id(0) {
@@ -109,156 +112,141 @@ void SecureWorkerInternal::emitMessage(const char *message) {
 
 void SecureWorkerInternal::bootstrapMock(sgx_sealed_data_t *out, size_t out_size,
                                          const uint8_t *additional_data, size_t additional_data_size,
-                                         const uint8_t *clear, size_t data_size) {
+                                         const uint8_t *data, size_t data_size) {
 	{
-		const sgx_status_t status = duk_enclave_bootstrap_mock(enclave_id, out, out_size, additional_data, additional_data_size, clear, data_size);
+		const sgx_status_t status = duk_enclave_bootstrap_mock(enclave_id, out, out_size, additional_data, additional_data_size, data, data_size);
 		if (status != SGX_SUCCESS) throw sgx_error(status, "duk_enclave_bootstrap_mock");
 	}
 }
 
+Nan::Persistent<v8::Function> SecureWorkerInternal::constructor;
 
-v8::Handle<v8::Value> SecureWorkerInternal::New(const v8::Arguments &arguments) {
-	v8::HandleScope scope;
-	if (!arguments.IsConstructCall()) {
-		v8::ThrowException(v8::Exception::Error(v8::String::New("SecureWorkerInternal called not as a constructor")));
-		return scope.Close(v8::Undefined());
+NAN_METHOD(SecureWorkerInternal::New) {
+	if (!info.IsConstructCall()) {
+		return Nan::ThrowError("SecureWorkerInternal called not as a constructor");
 	}
-	if (!arguments[0]->IsString()) {
-		v8::ThrowException(v8::Exception::TypeError(v8::String::New("Argument error")));
-		return scope.Close(v8::Undefined());
+	if (!info[0]->IsString()) {
+		return Nan::ThrowTypeError("Argument error");
 	}
-	v8::String::Utf8Value arg0_utf8(arguments[0]);
+	Nan::Utf8String arg0_utf8(info[0]);
 	SecureWorkerInternal *secure_worker_internal;
 	try {
-		entry_info entry(arguments.This());
+		entry_info entry(info.This());
 		secure_worker_internal = new SecureWorkerInternal(*arg0_utf8);
 	} catch (sgx_error error) {
-		error.rethrow();
-		return scope.Close(v8::Undefined());
+		return error.rethrow();
 	}
 
-	secure_worker_internal->Wrap(arguments.This());
-	// Why doesn't this need scope.Close?
-	return arguments.This();
+	secure_worker_internal->Wrap(info.This());
+	info.GetReturnValue().Set(info.This());
 }
 
-v8::Handle<v8::Value> SecureWorkerInternal::Init(const v8::Arguments &arguments) {
-	v8::HandleScope scope;
-
-	if (!arguments[0]->IsString()) {
-		v8::ThrowException(v8::Exception::TypeError(v8::String::New("Argument error")));
-		return scope.Close(v8::Undefined());
+NAN_METHOD(SecureWorkerInternal::Init) {
+	if (!info[0]->IsString()) {
+		return Nan::ThrowTypeError("Argument error");
 	}
-	v8::String::Utf8Value arg0_utf8(arguments[0]);
-	SecureWorkerInternal *secure_worker_internal = node::ObjectWrap::Unwrap<SecureWorkerInternal>(arguments.This());
+	Nan::Utf8String arg0_utf8(info[0]);
+	SecureWorkerInternal *secure_worker_internal = Nan::ObjectWrap::Unwrap<SecureWorkerInternal>(info.This());
 	try {
-		entry_info entry(arguments.This());
+		entry_info entry(info.This());
 		secure_worker_internal->init(*arg0_utf8);
 	} catch (sgx_error error) {
-		error.rethrow();
-		return scope.Close(v8::Undefined());
+		return error.rethrow();
 	}
-	return scope.Close(v8::Undefined());
 }
 
-v8::Handle<v8::Value> SecureWorkerInternal::Close(const v8::Arguments &arguments) {
-	v8::HandleScope scope;
-	SecureWorkerInternal *secure_worker_internal = node::ObjectWrap::Unwrap<SecureWorkerInternal>(arguments.This());
+NAN_METHOD(SecureWorkerInternal::Close) {
+	SecureWorkerInternal *secure_worker_internal = Nan::ObjectWrap::Unwrap<SecureWorkerInternal>(info.This());
 	try {
-		entry_info entry(arguments.This());
+		entry_info entry(info.This());
 		secure_worker_internal->close();
 	} catch (sgx_error error) {
-		error.rethrow();
-		return scope.Close(v8::Undefined());
+		return error.rethrow();
 	}
-	return scope.Close(v8::Undefined());
 }
 
-v8::Handle<v8::Value> SecureWorkerInternal::EmitMessage(const v8::Arguments &arguments) {
-	v8::HandleScope scope;
-	if (!arguments[0]->IsString()) {
-		v8::ThrowException(v8::Exception::TypeError(v8::String::New("Argument error")));
-		return scope.Close(v8::Undefined());
+NAN_METHOD(SecureWorkerInternal::EmitMessage) {
+	if (!info[0]->IsString()) {
+		return Nan::ThrowTypeError("Argument error");
 	}
-	v8::String::Utf8Value arg0_utf8(arguments[0]);
-	SecureWorkerInternal *secure_worker_internal = node::ObjectWrap::Unwrap<SecureWorkerInternal>(arguments.This());
+	Nan::Utf8String arg0_utf8(info[0]);
+	SecureWorkerInternal *secure_worker_internal = Nan::ObjectWrap::Unwrap<SecureWorkerInternal>(info.This());
 	try {
-		entry_info entry(arguments.This());
+		entry_info entry(info.This());
 		secure_worker_internal->emitMessage(*arg0_utf8);
 	} catch (sgx_error error) {
-		error.rethrow();
-		return scope.Close(v8::Undefined());
+		return error.rethrow();
 	}
-	return scope.Close(v8::Undefined());
 }
 
-v8::Handle<v8::Value> SecureWorkerInternal::BootstrapMock(const v8::Arguments &arguments) {
-	v8::HandleScope scope;
-	SecureWorkerInternal *secure_worker_internal = node::ObjectWrap::Unwrap<SecureWorkerInternal>(arguments.This());
-	v8::Local<v8::Object> typed_array_bindings = v8::Object::New();
-	v8_typed_array::AttachBindings(typed_array_bindings);
+NAN_METHOD(SecureWorkerInternal::BootstrapMock) {
+	SecureWorkerInternal *secure_worker_internal = Nan::ObjectWrap::Unwrap<SecureWorkerInternal>(info.This());
 
 	size_t additional_data_size;
-	const void *additional_data;
-	if (arguments[0]->IsObject() && arguments[0].As<v8::Object>()->HasIndexedPropertiesInExternalArrayData()) {
-		additional_data_size = arguments[0].As<v8::Object>()->GetIndexedPropertiesExternalArrayDataLength() * v8_typed_array::SizeOfArrayElementForType(arguments[0].As<v8::Object>()->GetIndexedPropertiesExternalArrayDataType());
-		additional_data = arguments[0].As<v8::Object>()->GetIndexedPropertiesExternalArrayData();
+	const uint8_t *additional_data;
+	if (info[0]->IsArrayBuffer()) {
+	  Nan::TypedArrayContents<uint8_t> arg0_uint8(info[0]);
+		additional_data = *arg0_uint8;
+		additional_data_size = info[0].As<v8::Uint8Array>()->Length();
 	} else {
-		additional_data_size = 0;
 		additional_data = nullptr;
+		additional_data_size = 0;
 	}
 
 	size_t data_size;
-	const void *clear;
-	if (arguments[1]->IsObject() && arguments[1].As<v8::Object>()->HasIndexedPropertiesInExternalArrayData()) {
-		data_size = arguments[1].As<v8::Object>()->GetIndexedPropertiesExternalArrayDataLength() * v8_typed_array::SizeOfArrayElementForType(arguments[1].As<v8::Object>()->GetIndexedPropertiesExternalArrayDataType());
-		clear = arguments[1].As<v8::Object>()->GetIndexedPropertiesExternalArrayData();
+	const uint8_t *data;
+	if (info[1]->IsArrayBuffer()) {
+	  Nan::TypedArrayContents<uint8_t> arg1_uint8(info[1]);
+		data = *arg1_uint8;
+		data_size = info[1].As<v8::Uint8Array>()->Length();
 	} else {
+		data = nullptr;
 		data_size = 0;
-		clear = nullptr;
 	}
 
 	const size_t out_size = sgx_calc_sealed_data_size(additional_data_size, data_size);
-	v8::Local<v8::Value> arraybuffer_arguments[] = {v8::Integer::New(out_size)};
-	v8::Local<v8::Object> out_buffer = typed_array_bindings->Get(v8::String::NewSymbol("ArrayBuffer")).As<v8::Function>()->NewInstance(1, arraybuffer_arguments);
-	if (out_buffer->GetIndexedPropertiesExternalArrayDataType() != v8::kExternalUnsignedByteArray || static_cast<size_t>(out_buffer->GetIndexedPropertiesExternalArrayDataLength()) != out_size) {
-		v8::ThrowException(v8::Exception::TypeError(v8::String::New("out")));
-		return scope.Close(v8::Undefined());
-	}
-	void *out = out_buffer->GetIndexedPropertiesExternalArrayData();
+  v8::Local<v8::ArrayBuffer> out_buffer = v8::ArrayBuffer::New(v8::Isolate::GetCurrent(), out_size);
+  v8::Local<v8::Uint8Array> out_array = v8::Uint8Array::New(out_buffer, 0, out_size);
+
+  Nan::TypedArrayContents<uint8_t> out_uint8(out_array);
+
+	sgx_sealed_data_t *out = reinterpret_cast<sgx_sealed_data_t *>(*out_uint8);
 
 	try {
-		entry_info entry(arguments.This());
+		entry_info entry(info.This());
 		secure_worker_internal->bootstrapMock(
-			reinterpret_cast<sgx_sealed_data_t *>(out), out_size,
-			reinterpret_cast<const uint8_t *>(additional_data), additional_data_size,
-			reinterpret_cast<const uint8_t *>(clear), data_size
+			out, out_size,
+			additional_data, additional_data_size,
+			data, data_size
 		);
 	} catch (sgx_error error) {
-		error.rethrow();
-		return scope.Close(v8::Undefined());
+		return error.rethrow();
 	}
-	return scope.Close(out_buffer);
+	info.GetReturnValue().Set(out_buffer);
 }
 
-static void secureworker_internal_init(v8::Handle<v8::Object> exports, v8::Handle<v8::Object> module) {
-	v8::Local<v8::FunctionTemplate> function_template = v8::FunctionTemplate::New(SecureWorkerInternal::New);
-	function_template->SetClassName(v8::String::NewSymbol("SecureWorkerInternal"));
+static void secureworker_internal_init(v8::Local<v8::Object> exports) {
+	v8::Local<v8::FunctionTemplate> function_template = Nan::New<v8::FunctionTemplate>(SecureWorkerInternal::New);
+
+	function_template->SetClassName(Nan::New("SecureWorkerInternal").ToLocalChecked());
 	function_template->InstanceTemplate()->SetInternalFieldCount(1);
-	function_template->PrototypeTemplate()->Set(v8::String::NewSymbol("init"), v8::FunctionTemplate::New(SecureWorkerInternal::Init)->GetFunction());
-	function_template->PrototypeTemplate()->Set(v8::String::NewSymbol("close"), v8::FunctionTemplate::New(SecureWorkerInternal::Close)->GetFunction());
-	function_template->PrototypeTemplate()->Set(v8::String::NewSymbol("emitMessage"), v8::FunctionTemplate::New(SecureWorkerInternal::EmitMessage)->GetFunction());
-	function_template->PrototypeTemplate()->Set(v8::String::NewSymbol("bootstrapMock"), v8::FunctionTemplate::New(SecureWorkerInternal::BootstrapMock)->GetFunction());
-	function_template->PrototypeTemplate()->Set(v8::String::NewSymbol("handlePostMessage"), v8::Null());
-	module->Set(v8::String::NewSymbol("exports"), function_template->GetFunction());
+
+	Nan::SetPrototypeMethod(function_template, "init", SecureWorkerInternal::Init);
+	Nan::SetPrototypeMethod(function_template, "close", SecureWorkerInternal::Close);
+	Nan::SetPrototypeMethod(function_template, "emitMessage", SecureWorkerInternal::EmitMessage);
+	Nan::SetPrototypeMethod(function_template, "bootstrapMock", SecureWorkerInternal::BootstrapMock);
+	Nan::SetPrototypeTemplate(function_template, "handlePostMessage", Nan::Null());
+
+  SecureWorkerInternal::constructor.Reset(Nan::GetFunction(function_template).ToLocalChecked());
+
+  Nan::Set(exports, Nan::New("SecureWorkerInternal").ToLocalChecked(), Nan::GetFunction(function_template).ToLocalChecked());
 }
 
 void duk_enclave_post_message(const char *message) {
-	v8::HandleScope scope;
 	assert(thread_entry != nullptr);
-	v8::Local<v8::Value> handle_post_message = thread_entry->entrant->Get(v8::String::NewSymbol("handlePostMessage"));
+	v8::Local<v8::Value> handle_post_message = Nan::Get(thread_entry->entrant, Nan::New("handlePostMessage").ToLocalChecked()).ToLocalChecked();
 	if (!handle_post_message->IsFunction()) return;
-	v8::Local<v8::Value> arguments[] = {v8::String::New(message)};
+	v8::Local<v8::Value> arguments[] = {Nan::New<v8::String>(message).ToLocalChecked()};
 	handle_post_message.As<v8::Function>()->Call(thread_entry->entrant, 1, arguments);
 }
 
